63090e0e4208dfa4e76fe93dfb874b01658ec452
Andrey Gud <nosacred@gmail.com>
2022-01-25 16:02:51 +0700

    Counter Game

---
+++ counterGame.cpp
@@ -0,0 +1,75 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+
+/*
+ * Complete the 'counterGame' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts LONG_INTEGER n as parameter.
+ */
+
+string counterGame(long n) {
+	bool Louise = false;
+
+	while(n > 1) {
+		long taken = n;
+		long power = 0;
+		while(taken > 1) {
+			taken >>= 1;
+			power++;
+		}
+
+		long result = pow(2, power);
+		result < n ? n -= result : n -= result >> 1;
+		Louise = !Louise;
+	}
+
+	return Louise ? "Louise" : "Richard";
+}
+
+int main()
+{
+    string t_temp;
+    getline(cin, t_temp);
+
+    int t = stoi(ltrim(rtrim(t_temp)));
+
+    for (int t_itr = 0; t_itr < t; t_itr++) {
+        string n_temp;
+        getline(cin, n_temp);
+
+        long n = stol(ltrim(rtrim(n_temp)));
+
+        string result = counterGame(n);
+
+        cout << result << "\n";
+    }
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
